81e9f1a33717000d8853820ddc2a3c21e00a1210
suppanyu5-github <68688155+suppanyu5-github@users.noreply.github.com>
2020-10-22 18:25:11 +0700

    create migration permission of post

--- console/migrations/m201022_104839_create_premission_of_post.php
+++ console/migrations/m201022_104839_create_premission_of_post.php
@@ -12,7 +12,27 @@ class m201022_104839_create_premission_of_post extends Migration
      */
     public function safeUp()
     {
+        $auth = Yii::$app->authManager;
 
+        $create = $auth->createPermission('post-create');
+        $create->description = ('Create a post');
+        $auth->add($create);
+
+        $delete = $auth->createPermission('post-delete');
+        $delete->description = ('Delete a post');
+        $auth->add($delete);
+
+        $index = $auth->createPermission('post-index');
+        $index->description = ('List a post');
+        $auth->add($index);
+
+        $update = $auth->createPermission('post-update');
+        $update->description = ('Update a post');
+        $auth->add($update);
+
+        $view = $auth->createPermission('post-view');
+        $view->description = ('View a post');
+        $auth->add($view);
     }
 
     /**
@@ -20,9 +40,34 @@ class m201022_104839_create_premission_of_post extends Migration
      */
     public function safeDown()
     {
-        echo "m201022_104839_create_premission_of_post cannot be reverted.\n";
+        $auth = Yii::$app->authManager;
 
-        return false;
+        $create = $auth->getPermission('post-create');
+        if($create){
+            $auth->remove($create);
+        }
+
+        $delete = $auth->getPermission('post-delete');
+        if($delete){
+            $auth->remove($delete);
+        }
+
+        $index = $auth->getPermission('post-index');
+        if($index){
+            $auth->remove($index);
+        }
+
+        $update = $auth->getPermission('post-update');
+        if($update){
+            $auth->remove($update);
+        }
+
+        $view = $auth->getPermission('post-view');
+        if($view){
+            $auth->remove($view);
+        }
+
+        return true;
     }
 
     /*
